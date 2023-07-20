c982c0a63de4decd1edb25a73005936ee8e4a555
Stu Kennedy <info@continuata.com>
2023-07-20 09:57:06 +0100

    fix LayoutFunction Env type

--- index.ts
+++ index.ts
@@ -2,15 +2,22 @@ const HEADERS = { headers: { 'content-type': 'text/html;charset=UTF-8' } };
 
 export const html = String.raw;
 export const htmlResponse = (dom: string) => new Response(dom, HEADERS);
-export interface LayoutFunction {
-  (props: { children: string; request?: Request; params?: Params<any> }):
-    | string
-    | Promise<string>;
-}
+
+export type LayoutFunction<Env = any, Params extends string = any> = ({
+  children,
+  request,
+  params,
+  env,
+}: {
+  children: string;
+  request?: Request;
+  params?: Params;
+  env?: Env;
+}) => string | Promise<string>;
 
 export const applyLayout =
   (layout: LayoutFunction): PagesFunction =>
-  async ({ request, next }) => {
+  async ({ request, env, params, next }) => {
     const url = new URL(request.url);
     const method = request.method;
     if (
@@ -24,7 +31,10 @@ export const applyLayout =
     const response = await next();
     const children = await response.text();
     if (response.ok) {
-      return new Response(await layout({ children, request }), response);
+      return new Response(
+        await layout({ children, request, params, env }),
+        response
+      );
     } else {
       return new Response('NOT FOUND', response);
     }
